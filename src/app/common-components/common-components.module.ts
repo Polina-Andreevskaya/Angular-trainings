15818c30fe93c95c283b515b9106be3169c377e7
Palina Andreyeuskaya <andreevskaya.polina@gmail.com>
2017-11-26 18:12:20 +0300

    Module_1: create finder component

--- src/app/common-components/common-components.module.ts
+++ src/app/common-components/common-components.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { LogoComponent } from './logo/logo.component';
 import { FooterComponent } from './footer/footer.component';
+import { FinderComponent } from './finder/finder.component';
 
 @NgModule({
   imports: [
@@ -10,9 +11,8 @@ import { FooterComponent } from './footer/footer.component';
   ],
   exports: [
   	HeaderComponent,
-  	LogoComponent,
   	FooterComponent
   ],
-  declarations: [HeaderComponent, LogoComponent, FooterComponent]
+  declarations: [HeaderComponent, LogoComponent, FooterComponent, FinderComponent]
 })
 export class CommonComponentsModule { }
