21af61715160277319bf15b31958ff5ba3babc07
Palina Andreyeuskaya <andreevskaya.polina@gmail.com>
2017-12-03 23:04:29 +0300

    Module 2: change layout

--- src/app/common-components/common-components.module.ts
+++ src/app/common-components/common-components.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 import { HeaderComponent } from './header/header.component';
 import { LogoComponent } from './logo/logo.component';
 import { FooterComponent } from './footer/footer.component';
 import { FinderComponent } from './finder/finder.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   exports: [
   	HeaderComponent,
   	FooterComponent
   ],
-  declarations: [HeaderComponent, LogoComponent, FooterComponent, FinderComponent]
+  declarations: [HeaderComponent, LogoComponent, FooterComponent, FinderComponent, LoginComponent]
 })
 export class CommonComponentsModule { }
