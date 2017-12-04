9a5c229e3803819fcafe65d0fa881b5342d2d22b
Palina Andreyeuskaya <palina_andreyeuskaya@epam.com>
2017-12-04 16:49:05 +0300

    Module 2: add mockup and simple logic

--- src/app/pages/courses-page/courses-page.component.ts
+++ src/app/pages/courses-page/courses-page.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CourseModel} from "./course/course.model";
 
 @Component({
   selector: 'app-courses-page',
@@ -7,9 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CoursesPageComponent implements OnInit {
 
-  constructor() { }
+  protected courses: Array<CourseModel>;
+
+  constructor() {
+  }
 
   ngOnInit() {
+    this.courses = [
+      {
+        id: 1,
+        title: 'First course',
+        date: new Date(),
+        duration: 120,
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      },
+      {
+        id: 2,
+        title: 'Second course',
+        date: new Date(),
+        duration: 95,
+        description: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+      },
+      {
+        id: 3,
+        title: 'Third course',
+        date: new Date(),
+        duration: 110,
+        description: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
+      },
+    ];
   }
+
+  onElementDeleted(elementId: number) {
+    console.log(elementId);
+  }
+
 
 }
